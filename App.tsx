523768e3d7d5223d7151dc85bf5436dcb42fd484
omarbougarne <omarbougarne98@gmail.com>
2025-09-03 17:23:19 +0100

    Feat adding folder structure

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      ghjk
+      <Text>Welcome to AniBuddy!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
